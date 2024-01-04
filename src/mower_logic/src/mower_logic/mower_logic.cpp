a7f934cd4426b4ab2a52ced4676b386f465ea1ae
astrizhkin <astrizhkin@biarum.com>
2024-01-04 23:40:54 +0300

    logging

--- src/mower_logic/src/mower_logic/mower_logic.cpp
+++ src/mower_logic/src/mower_logic/mower_logic.cpp
@@ -417,10 +417,7 @@ void checkSafety(const ros::TimerEvent &timer_event) {
         last_status.front_right_esc_status.status <= mower_msgs::ESCStatus ::ESC_STATUS_ERROR || 
         last_status.front_left_esc_status.status <= mower_msgs::ESCStatus ::ESC_STATUS_ERROR) {
         ROS_ERROR_STREAM(
-                "EMERGENCY: at least one motor control errored. errors RL: " << last_status.rear_left_esc_status
-                                                                << ", RR: " << last_status.rear_right_esc_status
-                                                                << ", FL: " << last_status.front_left_esc_status
-                                                                << ", FR: " << last_status.front_right_esc_status);
+                "EMERGENCY: at least one motor control errored. errors RL: " << last_status.rear_left_esc_status << ", RR: " << last_status.rear_right_esc_status << ", FL: " << last_status.front_left_esc_status << ", FR: " << last_status.front_right_esc_status);
         setEmergencyMode(true);
         return;
     }
