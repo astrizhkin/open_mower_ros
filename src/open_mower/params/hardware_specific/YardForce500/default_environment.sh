ef99b674af3c8ba490d0d5ff6168c083b6ca9090
astrizhkin <astrizhkin@biarum.com>
2025-05-24 14:47:46 +0300

    remove unused vars

--- src/open_mower/params/hardware_specific/YardForce500/default_environment.sh
+++ src/open_mower/params/hardware_specific/YardForce500/default_environment.sh
@@ -1,6 +1,2 @@
-# Set default GPS antenna offset
-#export OM_ANTENNA_OFFSET_X=${OM_ANTENNA_OFFSET_X:-0.3}
-#export OM_ANTENNA_OFFSET_Y=${OM_ANTENNA_OFFSET_Y:-0.0}
-
 # Set default ticks/m
 export OM_WHEEL_TICKS_PER_M=${OM_WHEEL_TICKS_PER_M:-1600.0}
